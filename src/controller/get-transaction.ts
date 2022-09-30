9ad557c29e646762aa16d0d40dabff15239d81ec
erik-tavares <erik_gabrieltavares@hotmail.com>
2022-09-29 22:31:01 -0300

    transação adicionada

--- src/controller/get-transaction.ts
+++ src/controller/get-transaction.ts
@@ -4,10 +4,17 @@ import { UserDb } from "../db/users";
 export class GetTransactionUserController {
   getTransaction(request: Request, response: Response) {
     const { userId, id } = request.params;
+    if (!userId || id) {
+      return response
+        .status(404)
+        .json({ Error: "Usuário ou Transação não encontrado!" });
+    }
     const transaction = UserDb.find(
       (user) => user.id === userId
     )?.transactions.find((item) => item.id === id);
-    console.log(transaction);
+    if (!transaction) {
+      return response.status(404).json({ Error: "Transação não encontrada!" });
+    }
     return response.status(200).json(transaction);
   }
 }
